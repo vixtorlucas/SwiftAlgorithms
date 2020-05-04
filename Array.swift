836e585af37d7d64f477ca0f0f89ae5668cc045f
Victor Lucas <victorlucasdeodato@gmail.com>
2020-05-03 20:28:34 -0400

    extension created following comparable protocols

--- Array.swift
+++ Array.swift
@@ -1,18 +1,17 @@
-var array = [23,4,1,3,9,5,0,17,22,11,10,2]
 
-print(array.count)
-var index = 0
-for i in 1..<array.count{
-    let key = array[i]
-    index = i - 1 
-    while index > -1 && array[index] > key{
-        array[index + 1] = array[index]
-        index = index - 1
+extension Array where Element:Comparable{
+    func insertionSort() -> Array{
+        var index = 0
+        var array = self
+        for i in 1..<array.count{
+            let key = array[i]
+            index = i - 1
+            while index > -1 && array[index] > key{
+                array[index + 1] = array[index]
+                index = index - 1
+            }
+            array[index + 1] = key
+        }
+        return array
     }
-    
-    array[index + 1] = key
-    print(array)
 }
-
-print(array)
-
